0892d8ca8f8ee7414847a3a4002c0dc38d9b9ed3
Vinícius <viniciusmale91@gmail.com>
2026-01-27 16:52:16 -0300

    Criação de entidade para o banco de dados

---
+++ src/main/java/projeto/CadastroClientes/ClienteModel.java
@@ -0,0 +1,46 @@
+package projeto.CadastroClientes;
+
+import jakarta.persistence.*;
+
+//Entity tranforma uma classe em uma entidade no DB
+@Entity
+@Table(name = "tb_cliente")
+public class ClienteModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public ClienteModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
