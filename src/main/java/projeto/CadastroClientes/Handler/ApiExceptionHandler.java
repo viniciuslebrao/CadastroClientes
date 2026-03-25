de71a5e30ca51864be6492f22eb02775419a8e8b
Vinícius <viniciusmale91@gmail.com>
2026-03-25 16:57:26 -0300

    feat: Criação de ExceptionHandler para tratar exceções

---
+++ src/main/java/projeto/CadastroClientes/Handler/ApiExceptionHandler.java
@@ -0,0 +1,15 @@
+package projeto.CadastroClientes.Handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(EntidadeNaoEncontradaException.class)
+    public ResponseEntity<?> handleEntidadeNaoEncontrada(EntidadeNaoEncontradaException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+}
---
+++ src/main/java/projeto/CadastroClientes/Handler/EntidadeNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package projeto.CadastroClientes.Handler;
+
+public class EntidadeNaoEncontradaException extends Throwable {
+    public EntidadeNaoEncontradaException(String mensagem){
+        super(mensagem);
+    }
+}
